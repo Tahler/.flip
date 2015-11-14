53f3db2dd82212a6c3637c472a334d18d8b6188b
Tyler Berry <tberry@student.neumont.edu>
2015-11-13 19:19:44 -0700

    pen and pencil setup but not finishe

--- dotFlip/dotFlip/MainWindow.xaml.cs
+++ dotFlip/dotFlip/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Ink;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace dotFlip
 {
@@ -24,11 +12,11 @@ namespace dotFlip
         public MainWindow()
         {
             InitializeComponent();
-            stickyNote.DefaultDrawingAttributes = new DrawingAttributes
-            {
-                Color = Colors.Black,
-                FitToCurve = true,
-            };
+            //stickyNote.DefaultDrawingAttributes = new DrawingAttributes
+            //{
+            //    Color = Colors.Black,
+            //    FitToCurve = true,
+            //};
         }
     }
 }
---
+++ dotFlip/dotFlip/Tools/Pencil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Input.StylusPlugIns;
+using System.Windows.Media;
+
+namespace dotFlip.Tools
+{
+    public class Pencil : ITool
+    {
+        Brush brush;
+        System.Windows.Media.Pen pen;
+
+        public Color Color { get; set; }
+        public double Thickness { get; set; }
+
+        public Pencil()
+        {
+            // Create the Brush and Pen used for drawing.
+            brush = new LinearGradientBrush(Colors.Red, Colors.Blue, 20d);
+            pen = new System.Windows.Media.Pen(brush, 2d);
+        }
+
+        public void Draw(StylusPoint point, DrawingContext drawingContext)
+        {
+            double radius = point.PressureFactor * 10d;
+            // create a bunch of small alpha-cized ellipses around point
+            drawingContext.DrawEllipse(brush, pen, (Point)point, radius, radius);
+        }
+    }
+}
