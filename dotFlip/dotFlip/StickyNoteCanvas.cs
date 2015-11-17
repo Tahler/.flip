fa4335c7fb602fd2abe9f9852078324d9a0e4864
Tyler Berry <tberry@student.neumont.edu>
2015-11-16 17:45:05 -0700

    not working yet

--- dotFlip/dotFlip/StickyNoteCanvas.cs
+++ dotFlip/dotFlip/StickyNoteCanvas.cs
@@ -21,11 +21,24 @@ namespace dotFlip
             MouseMove += StickyNoteCanvas_MouseMove;
         }
 
+        PathGeometry currentPathGeometry;
+        PathFigure currentPathFigure;
+
         private void StickyNoteCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ButtonState == MouseButtonState.Pressed)
             {
                 previousPoint = e.GetPosition(this);
+
+                this.Children.Add(new Path
+                {
+                    Stroke = CurrentTool.Brush,
+                    StrokeThickness = CurrentTool.Thickness
+                });
+
+                currentPathGeometry = new PathGeometry();
+                currentPathFigure = new PathFigure {StartPoint = previousPoint};
+                currentPathGeometry.Figures.Add(currentPathFigure);
             }
         }
 
@@ -35,25 +48,27 @@ namespace dotFlip
             {
                 Point currentPoint = e.GetPosition(this);
 
-                Line line = new Line
-                {
-                    Stroke = CurrentTool.Brush,
-                    StrokeThickness = CurrentTool.Thickness,
-                    X1 = previousPoint.X,
-                    Y1 = previousPoint.Y,
-                    X2 = currentPoint.X,
-                    Y2 = currentPoint.Y,
+                //Line line = new Line
+                //{
+                //    Stroke = CurrentTool.Brush,
+                //    StrokeThickness = CurrentTool.Thickness,
+                //    X1 = previousPoint.X,
+                //    Y1 = previousPoint.Y,
+                //    X2 = currentPoint.X,
+                //    Y2 = currentPoint.Y,
                     
-                    // potential fix for line cracks
-                    StrokeDashCap = PenLineCap.Round,
-                    StrokeStartLineCap = PenLineCap.Round,
-                    StrokeEndLineCap = PenLineCap.Round
-                };
+                //    // potential fix for line cracks
+                //    StrokeDashCap = PenLineCap.Round,
+                //    StrokeStartLineCap = PenLineCap.Round,
+                //    StrokeEndLineCap = PenLineCap.Round
+                //};
 
+                LineSegment segment = new LineSegment(currentPoint, true);
+                currentPathFigure.Segments.Add(segment);
+                currentPathGeometry.Figures.Add(currentPathFigure);
+                ((Path)Children[Children.Count - 1]).Data = currentPathGeometry;
 
                 previousPoint = currentPoint;
-
-                this.Children.Add(line);
             }
         }
     }
--- dotFlip/dotFlip/Tools/Pencil.cs
+++ dotFlip/dotFlip/Tools/Pencil.cs
@@ -30,19 +30,21 @@ namespace dotFlip.Tools
 
         public Pencil()
         {
-            Color = Colors.Black;
+            Color = Colors.Gray;
             Thickness = 5;
 
             GeometryDrawing geometryDrawing = new GeometryDrawing();
             geometryDrawing.Brush = Brushes.Gray;
             geometryDrawing.Pen = new System.Windows.Media.Pen(Brushes.Gray, 1);
 
+            //GeometryGroup ellipses = new GeometryGroup();
+            //ellipses.Children.Add(new RectangleGeometry(new Rect(new Point(0,45), new Point(100,89))));
+            //// ellipses.Children.Add(new RectangleGeometry(new Rect(new Point(0, 100), new Point(25, 89))));
+
             GeometryGroup ellipses = new GeometryGroup();
-            ellipses.Children.Add(new RectangleGeometry(new Rect(new Point(0,45), new Point(100,89))));
-           // ellipses.Children.Add(new RectangleGeometry(new Rect(new Point(0, 100), new Point(25, 89))));
-
-
-
+            ellipses.Children.Add(new EllipseGeometry(new Point(25, 50), 12.5, 25));
+            ellipses.Children.Add(new EllipseGeometry(new Point(50, 50), 12.5, 25));
+            ellipses.Children.Add(new EllipseGeometry(new Point(75, 50), 12.5, 25));
 
             geometryDrawing.Geometry = ellipses;
 
