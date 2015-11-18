7aef15d2ac6b434e8b8cc171500fe6587a23899b
Tyler Berry <tberry@student.neumont.edu>
2015-11-17 21:49:51 -0700

    eraser now centers

---
+++ dotFlip/dotFlip/Tools/Eraser.cs
@@ -0,0 +1,24 @@
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace dotFlip.Tools
+{
+    public class Eraser : ITool
+    {
+        // Interesting issue: no use for color
+        public Color Color { get; set; }
+
+        public double Thickness { get; set; }
+
+        public Shape Shape => new Ellipse
+        {
+            Width = Thickness,
+            Height = Thickness,
+        };
+
+        public Eraser()
+        {
+            Thickness = 10;
+        }
+    }
+}
