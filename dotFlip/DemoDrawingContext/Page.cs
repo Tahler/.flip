3a48de8d05879b3fb24dcc9a789166136ad638e4
Tyler Berry <tberry@student.neumont.edu>
2015-11-19 20:39:46 -0700

    next and previous page works

---
+++ dotFlip/DemoDrawingContext/Page.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DemoDrawingContext
+{
+    public class Page : Panel
+    {
+        private IList<Visual> visuals;
+
+        public Page()
+        {
+            visuals = new List<Visual>();
+
+            DrawingVisual path = new DrawingVisual();
+            using (var context = path.RenderOpen())
+            {
+                context.DrawEllipse(Brushes.Black, null, new Point(50, 50), 20, 20);
+            }
+            visuals.Add(path);
+        }
+
+        public void DrawAt(Point point) // will probably need to pass the tool
+        {
+            Console.WriteLine("added");
+            DrawingVisual path = new DrawingVisual();
+            using (var context = path.RenderOpen())
+            {
+                context.DrawEllipse(Brushes.Black, null, point, 20, 20);
+            }
+            visuals.Add(path);
+        }
+
+        /// <summary>
+        /// Overrides Panel's OnRender, to draw the background color or, if the background is null, a transparent rectangle. 
+        /// Filling the window with this rectangle allows for Events such as MouseDown to be triggered.
+        /// </summary>
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            Brush background = Background ?? Brushes.Transparent;
+            drawingContext.DrawRectangle(background, null, new Rect(RenderSize));
+        }
+
+        public new int VisualChildrenCount => visuals.Count;
+
+        public new Visual GetVisualChild(int index)
+        {
+            Console.WriteLine("calling");
+            return visuals[index];
+        }
+    }
+}
---
+++ dotFlip/DemoMultiPanel/Flipbook.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace DemoMultiPanel
+{
+    public class Flipbook
+    {
+        private IList<Page> pages;
+
+        public event PageChangedHandler CurrentPageChanged = delegate { };
+
+        private Page currentPage;
+
+        public Page CurrentPage
+        {
+            get { return currentPage; }
+            set
+            {
+                currentPage = value;
+                CurrentPageChanged(currentPage);
+            }
+        }
+
+        public Flipbook()
+        {
+            CurrentPage = new Page(Brushes.AliceBlue);
+            pages = new List<Page> {CurrentPage};
+            pages.Add(new Page(Brushes.Yellow));
+        }
+
+        public void NextPage()
+        {
+            int currentIndex = pages.IndexOf(CurrentPage);
+            this.CurrentPage = pages[currentIndex + 1];
+        }
+
+        public void PreviousPage()
+        {
+            int currentIndex = pages.IndexOf(CurrentPage);
+            this.CurrentPage = pages[currentIndex - 1];
+        }
+    }
+
+    public delegate void PageChangedHandler(Page currentPage);
+}
