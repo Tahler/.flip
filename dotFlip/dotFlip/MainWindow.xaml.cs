947771111a04293f3b0fa66e24818314c0f59161
Tyler Berry <tberry@student.neumont.edu>
2015-11-20 14:17:07 -0700

    cleaned up unnecessary usings

--- dotFlip/dotFlip/MainWindow.xaml.cs
+++ dotFlip/dotFlip/MainWindow.xaml.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
 using System.Windows.Media;
-using dotFlip.Tools;
-using Pen = dotFlip.Tools.Pen;
 
 namespace dotFlip
 {
