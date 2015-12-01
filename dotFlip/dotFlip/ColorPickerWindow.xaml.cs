7e6be946aad1d4c0867490ec006e1bf6598b28ba
enochfin <enochfinley@gmail.com>
2015-11-30 22:27:31 -0700

    i forgot to add the files

---
+++ dotFlip/dotFlip/ColorPickerWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dotFlip
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class ColorPickerWindow : Window
+    {
+        public ColorPickerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Tool_ClrPicker_LostFocus(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
