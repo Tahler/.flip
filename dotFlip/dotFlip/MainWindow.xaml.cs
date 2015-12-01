5fa3d3904bf0bf89e75d060a4df2dc16e9f6281c
Carver Anglin <anglincarver@gmail.com>
2015-11-30 19:18:06 -0700

    images for status bar in place

--- dotFlip/dotFlip/MainWindow.xaml.cs
+++ dotFlip/dotFlip/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace dotFlip
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txtPageNum.Text = "6";
+        }
     }
 }
