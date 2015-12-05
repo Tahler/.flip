86cd386a5f912ce21207beed8c0f43b81f9cfb11
Tyler Berry <tberry@student.neumont.edu>
2015-12-04 17:44:35 -0700

    ICommand rather than RoutedCommand for cleanliness

--- dotFlip/dotFlip/Commands.cs
+++ dotFlip/dotFlip/Commands.cs
@@ -9,13 +9,13 @@ namespace dotFlip
 {
     public static class Commands
     {
-        public static RoutedCommand PreviousPage { get; } = new RoutedCommand();
-        public static RoutedCommand NextPage { get; } = new RoutedCommand();
-        public static RoutedCommand Play { get; } = new RoutedCommand();
-        public static RoutedCommand CopyPreviousPage { get; } = new RoutedCommand();
-        public static RoutedCommand ToggleGhostStrokes { get; } = new RoutedCommand();
-        public static RoutedCommand ClearPage { get; } = new RoutedCommand();
-        public static RoutedCommand DeletePage { get; } = new RoutedCommand();
-        public static RoutedCommand Restart { get; } = new RoutedCommand();
+        public static ICommand PreviousPage { get; } = new RoutedCommand();
+        public static ICommand NextPage { get; } = new RoutedCommand();
+        public static ICommand Play { get; } = new RoutedCommand();
+        public static ICommand CopyPreviousPage { get; } = new RoutedCommand();
+        public static ICommand ToggleGhostStrokes { get; } = new RoutedCommand();
+        public static ICommand ClearPage { get; } = new RoutedCommand();
+        public static ICommand DeletePage { get; } = new RoutedCommand();
+        public static ICommand Restart { get; } = new RoutedCommand();
     }
 }
